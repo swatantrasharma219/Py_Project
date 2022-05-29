0c8518fa00489b61fbd34936015a38cf0d18b8ad
Swatantra Sharma <105961629+swatantrasharma219@users.noreply.github.com>
2022-05-29 22:59:59 +0530

    Create circle_area.py

---
+++ math_module/circle_area.py
@@ -0,0 +1,7 @@
+from math import*           # importing module
+
+r = int(input('Enter the radius : '))
+
+ar = pi * r ** 2            # here pi represents the value pie(3.14)
+
+print('The area of the circle is =',round(ar,2))    # we use round for two decimal points
