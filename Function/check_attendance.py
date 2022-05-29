65c9f624719c3152a4535fd590799a9a0f72bf9c
Swatantra Sharma <105961629+swatantrasharma219@users.noreply.github.com>
2022-05-29 19:41:31 +0530

    Create check_attendance.py

---
+++ Function/check_attendance.py
@@ -0,0 +1,13 @@
+def swat(a):
+
+	a = [1,4,5,2]   # including roll numbber list 	if b in a:      # checking if b is present in a's any index
+
+		print('Present')
+
+	else:
+
+		print('Absent')
+
+b = int(input('Enter roll : '))
+
+ank(b)            # function calling 
