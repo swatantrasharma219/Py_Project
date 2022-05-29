9fafb138536898dfad257f3dc905ef1630a5c127
Swatantra Sharma <105961629+swatantrasharma219@users.noreply.github.com>
2022-05-29 23:42:34 +0530

    Create check_file.py

---
+++ Exception Handling/check_file.py
@@ -0,0 +1,21 @@
+try:    
+
+	a = open('hi.txt','r')    # try open hi.txt fileexcept IOError:            
+
+	print('The file does not exits')    
+
+else:    
+
+	print('The file is exists')    
+
+a.close()                   # cosing the file
+
+	
+                                        
+                                         
+                               
+
+
+
+	  
+
