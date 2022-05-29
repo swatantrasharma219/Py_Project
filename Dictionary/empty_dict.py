ee48376b53039515bd67de161d03f970c97da8f3
Swatantra Sharma <105961629+swatantrasharma219@users.noreply.github.com>
2022-05-29 18:55:04 +0530

    Create empty_dict.py

---
+++ Dictionary/empty_dict.py
@@ -0,0 +1,15 @@
+a = {}
+
+print('The original dictionary : ',a)
+
+b = not a #use of not operator to check if dictionary is empty 
+
+print('Is the dictionary empty ? : ',b)#it prints true ,beacause the dicionary is empty 
+
+a = {1 : 'Hello'}
+
+print('The original dictionary : ',a)
+
+b = not a #use of not operator to check if dictionary is empty 
+
+print('Is the dictionary empty ? : ',b)#it prints false ,beacause the dicionary has key : value pair  
