530ebcace2d54deaa11a4640c574bbe18f499c85
Swatantra Sharma <105961629+swatantrasharma219@users.noreply.github.com>
2022-05-29 19:53:09 +0530

    Create calc_power.py

---
+++ Function/calc_power.py
@@ -0,0 +1,7 @@
+power = lambda x,y : x ** y # defining lambda function , declaring the variable x,y annd these are local variable in lambda function 
+
+a = int(input('Enter a number : '))
+
+b = int(input('Enter the number power : '))
+
+print('The number is :',power(a,b)) # function calling , a assign to x and y assign to y
