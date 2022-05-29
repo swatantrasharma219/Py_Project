5b4a02692e42ca0482d1c5d1d81d05fb79f03f42
Swatantra Sharma <105961629+swatantrasharma219@users.noreply.github.com>
2022-05-29 23:52:41 +0530

    Check_ele_index.py

---
+++ Exception Handling/Check_ele_index.py
@@ -0,0 +1,13 @@
+a = [4,5,6]
+
+print('The list is :',a)
+
+try:
+
+	b = int(input('Enter the index : '))	
+      
+        print('The number is :',a[b]) 
+
+except:
+
+	print ('The index is not available.')
