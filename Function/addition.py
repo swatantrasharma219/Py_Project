e50484d76bd7a89e92350d9f8e37dda6e77863b1
Swatantra Sharma <105961629+swatantrasharma219@users.noreply.github.com>
2022-05-29 19:44:22 +0530

    Create addition.py

---
+++ Function/addition.py
@@ -0,0 +1,7 @@
+def addition(a , b): #def is use for declare a user defined function
+
+	print('Sum of',a,'and',b,'=',a+b)x = float(input('Enter first number : '))
+
+y = float(input('Enter second number : '))
+
+addition(x , y) #passing the elements through user defined function
