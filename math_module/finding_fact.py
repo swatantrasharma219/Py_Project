8cb6abd6e23689b05a405c49c31f4b826bc25763
Swatantra Sharma <105961629+swatantrasharma219@users.noreply.github.com>
2022-05-29 23:02:56 +0530

    Create finding_fact.py

---
+++ math_module/finding_fact.py
@@ -0,0 +1,7 @@
+from math import*         # importing math module
+
+a = int(input('Enter a number : '))
+
+b = factorial(a)          # it has a factorial funtion which returns the factorial of a number
+
+print('Factorial of',a,'=',b)
