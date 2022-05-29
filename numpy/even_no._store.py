b3507c529653beafad91e21d53c46a32a49768d8
Swatantra Sharma <105961629+swatantrasharma219@users.noreply.github.com>
2022-05-29 20:27:01 +0530

    Create even_no._store.py

---
+++ numpy/even_no._store.py
@@ -0,0 +1,13 @@
+from numpy import*          # importing module
+
+a = []
+
+s,e = map(int,input('Enter starting and ending range : ').split())
+
+if s % 2 != 0:
+
+	s += 1for s in range(s,c,2):
+
+	a.append(s)
+
+print(array(a))
