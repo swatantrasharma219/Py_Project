ccaf4d6c47ba464f1f38ba47d500490090190fc6
Swatantra Sharma <105961629+swatantrasharma219@users.noreply.github.com>
2022-05-29 23:19:50 +0530

    Create random_number.py

---
+++ Random Module/random_number.py
@@ -0,0 +1,8 @@
+from random import*         # importing module
+
+a = [4,5,6,7,8]
+
+print('The list is :',a)
+
+print('Printing a random number from list :',choice(a)) # choice() picks a random number from the given list
+
